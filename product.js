536abb3339fa4eeee74668637f021562973b6edd
AjayGajula <69115878+AjayGajula@users.noreply.github.com>
2020-12-06 15:34:04 +0530

    Update product.js

--- product.js
+++ product.js
@@ -120,6 +120,17 @@ function products(data) {
 
   onLoadcartNumber();
 
+  
+  function navSlide() {
+    var burger = document.querySelector(".burger");
+    var nav = document.querySelector(".menu-items");
+  
+    burger.addEventListener("click", function() {
+      nav.classList.toggle("nav-active");
+      burger.classList.toggle("toggle");
+    });
+  }
+  navSlide();
 
 
   for (var i = 0; i < data.photos.length; i++) {
