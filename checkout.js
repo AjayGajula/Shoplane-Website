c7a133a5cc64ee53d4ca57acd115b27f6379592a
AjayGajula <69115878+AjayGajula@users.noreply.github.com>
2020-12-08 10:41:43 +0530

    Update checkout.js

--- checkout.js
+++ checkout.js
@@ -21,13 +21,10 @@ function navSlide() {
 navSlide();
 
 var myLocalStorage = JSON.parse(localStorage.getItem("productIncart"));
-var totalAmount = parseInt(localStorage.getItem("totalAmount"));
 
 var costStorage = JSON.parse(localStorage.getItem("totalAmount"));
 var countValue = JSON.parse(localStorage.getItem("cartNumbers") || 0);
-
 $("#number-of-item").text(countValue);
-$("#total-amount").text(costStorage);
 
 function productContainer(productDetails) {
   $(".cart-items").append(
@@ -42,16 +39,92 @@ function productContainer(productDetails) {
           .attr("class", "details")
           .append(
             $("<h3>").text(productDetails.title),
-            $("<p>").text("x" + productDetails.count),
-            $("<p>").text("Amount: " + productDetails.price)
+            $("<p>")
+              .attr("id", "count" + productDetails.id)
+              .text("x" + productDetails.count),
+            $("<p>")
+              .attr("id", "tprice" + productDetails.id)
+              .text("Amount: " + productDetails.price)
+          ),
+        $("<div>")
+          .attr("id", "btns")
+          .append(
+            $("<button>")
+              .attr("id", "leftbtn" + productDetails.id)
+              .text("-"),
+            $("<p>")
+              .attr("id", "value" + productDetails.id)
+              .text(productDetails.count),
+            $("<button>")
+              .attr("id", "rightbtn" + productDetails.id)
+              .text("+")
           )
       )
   );
+  $(`#leftbtn${productDetails.id}`).click(function () {
+    decreaseNumber(productDetails, countValue);
+  });
+  $(`#rightbtn${productDetails.id}`).click(function () {
+    increaseNumber(productDetails, costStorage);
+  });
 }
 
 for (var i = 0; i < myLocalStorage.length; i++) {
-  productContainer(myLocalStorage[i], costStorage);
+  productContainer(myLocalStorage[i], costStorage, countValue);
 }
+
+function decreaseNumber(productDetails) {
+  if (productDetails.count == 0) {
+    alert("Negative values Not Allowed");
+  }
+  if (productDetails.count > 0) {
+    productDetails.count = productDetails.count - 1;
+    $(`#count${productDetails.id}`).text("x" + productDetails.count);
+    $(`#value${productDetails.id}`).text(productDetails.count);
+    var productNumber = localStorage.getItem("cartNumbers");
+    productNumber = parseInt(productNumber);
+    if (productNumber >= 0) {
+      localStorage.setItem("cartNumbers", productNumber - 1);
+      document.querySelector("#cart span").textContent = productNumber - 1;
+      $("#number-of-item").text(productNumber - 1);
+    }
+
+    var totalCost = localStorage.getItem("totalAmount");
+    if (totalCost != null) {
+      totalCost = parseInt(totalCost);
+      var amount = totalCost - productDetails.price;
+      $("#total-amount").text(amount);
+      localStorage.setItem("totalAmount", amount);
+    }
+  }
+  var productIncart = JSON.stringify(myLocalStorage);
+  localStorage.setItem("productIncart", productIncart);
+}
+
+function increaseNumber(productDetails) {
+  productDetails.count = productDetails.count + 1;
+  $(`#count${productDetails.id}`).text("x" + productDetails.count);
+  $(`#value${productDetails.id}`).text(productDetails.count);
+  var productNumber = localStorage.getItem("cartNumbers");
+  productNumber = parseInt(productNumber);
+  if (productNumber >= 0) {
+    localStorage.setItem("cartNumbers", productNumber + 1);
+    document.querySelector("#cart span").textContent = productNumber + 1;
+    $("#number-of-item").text(productNumber + 1);
+  }
+
+  var totalCost = localStorage.getItem("totalAmount");
+  if (totalCost != null) {
+    totalCost = parseInt(totalCost);
+    var amount = totalCost + productDetails.price;
+    $("#total-amount").text(amount);
+    localStorage.setItem("totalAmount", amount);
+  }
+  var productIncart = JSON.stringify(myLocalStorage);
+  localStorage.setItem("productIncart", productIncart);
+}
+
+$("#total-amount").text(costStorage);
 
 var placeOrder = document.getElementById("place-order");
 
